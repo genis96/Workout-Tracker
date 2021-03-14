e81d6558d3a0941453167e88705b878bc33e0120
Genis Bardales <genis96@yahoo.com>
2021-03-14 15:45:40 -0400

    work on structure.. having issues viewing on localhost

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,19 @@
 const path = require("path");
 const router = require("express").Router();
 
+// home route
 router.get("/", (req, res) => {
-    res.sendFile()
-})
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 module.exports = router;
