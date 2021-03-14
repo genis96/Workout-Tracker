530b0eeae97d12017601cb79f9145082799687a6
Genis Bardales <genis96@yahoo.com>
2021-03-14 15:41:58 -0400

    work on structure.. having issues viewing on localhost

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const htmlRoutes = require("./routes/html-routes");
-const apiRoutes = require("./routes/api-routes");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-
-// const db = mongoose.connection;
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(morgan("dev"));
-app.use(htmlRoutes);
-app.use(apiRoutes);
+app.use(express.urlencoded({extended: true}))
 
+// Mongoose connection syntax //
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }
+);
 
-// routes
-// app.use(require("./routes/api.js"));
-// app.use(require("./routes/html.js"));
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`Success! App is running on port ${PORT}!`);
 });
-
-module.exports = app;
