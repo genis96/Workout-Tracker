0afe38a9d0fef83814106286e98a1c4071311e0d
Genis Bardales <genis96@yahoo.com>
2021-03-14 17:36:57 -0400

    put requests for updates

--- routes/api.js
+++ routes/api.js
@@ -15,10 +15,15 @@ router.put("/api/workouts/:id", (req, res) => {
     Workout.findOneAndUpdate(
         { _id: req.params.id }, //specified id
         {
-            $inc: {totalDuration: req.body.duration},
+            $inc: { totalDuration: req.body.duration },
             $push: { exercises: req.body }
-        }
-    )
+        }, 
+        { new: true }
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
 });
 
 //POST - CREATING WORKOUTS
